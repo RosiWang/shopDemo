c98a17c15eb0edce517c1db8287f18f8e0eade75
zisehuiyi2007 <rosiwang@163.com>
2018-03-29 19:51:25 +0800

    master

---
+++ src/components/VueCompt.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="contentDiv">  <!---只允许有一个最外层标签 ！-->
+      <p>test888888888888</p>
+  </div>
+</template>
+<script>
+  export default {
+    data: function () {
+      return {
+        message: 'vue好帅666666！'
+      }
+    }
+  }
+</script>
+<style>
+  #contentDiv{
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 10px;
+  }
+</style>
+
