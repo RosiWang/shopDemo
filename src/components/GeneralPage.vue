98ab6ce55c0c1352d0609bad7face49394e8761c
zisehuiyi2007 <rosiwang@163.com>
2018-04-02 19:32:26 +0800

    子页路由准备接入

--- src/components/GeneralPage.vue
+++ src/components/GeneralPage.vue
@@ -1,13 +1,28 @@
 <template xmlns:v-on="http://www.w3.org/1999/xhtml">
     <div>
-        <p>{{routerData.detailedData.content}}</p>
+        <div>
+            <p>{{routerData.detailedData.title}}</p>
+            <img v-bind:src="routerData.detailedData.src"/>
+            <br>
+            <button v-on:click="back">返回</button>
+            <button v-on:click="goDetail">详细信息</button>
+        </div>
         <router-view v-bind:router-detail="routerData"></router-view>
     </div>
 </template>
 <script>
     import router from '.././router'
     export default{
-        props:["routerData"]
+        props:["routerData"],
+        methods:{
+            back(){
+                router.push(this.$route.path.slice(0,9));
+            },
+            goDetail(){
+                console.log('goto detail page');
+              //  router.push({path:this.$route.path+"/detail"});
+            }
+        }
     }
     console.log('detailPage');
 </script>
